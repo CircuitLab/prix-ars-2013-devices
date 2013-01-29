7ae679b1ee639941c05054b99a675fa00e9570dd
moxus <moxuse@gmail.com>
2013-01-29 14:18:28 +0900

    wite(0xff)がなぜかエラー

--- Softmodem_Arduino_Uno/Softmodem_Arduino_Uno.ino
+++ Softmodem_Arduino_Uno/Softmodem_Arduino_Uno.ino
@@ -1,33 +1,30 @@
 #include <SoftModem.h>
- #include <ctype.h>
+#include <ctype.h>
 
- SoftModem modem;
+SoftModem modem;
 
- void setup()
- {
+void setup() {
    Serial.begin(57600);
    delay(1000);
    modem.begin();
- }
+}
 
- void loop()
- {
-   while(modem.available()){
-     int c = modem.read();
-     if(isprint(c)){
-       Serial.println((char)c);
-     }
-     else{
-       Serial.print("(");
-       Serial.print(c,HEX);
-       Serial.println(")");      
-     }
-   }
-   if(Serial.available()){
-     modem.write(0xff);
-     while(Serial.available()){
-       char c = Serial.read();
-       modem.write(c);
-     }
-   }
- } 
+void loop() {
+  while( modem.available() ) {
+   int c = modem.read();
+   if( isprint(c) ){
+      Serial.println( (char)c );
+    } else {
+      Serial.print("(");
+      Serial.print(c,HEX);
+      Serial.println(")");      
+    }
+  }
+  if( Serial.available() ) {
+    modem.wite(0xff);
+    while( Serial.available() ) {
+      char c = Serial.read();
+      modem.write(c);
+    }
+  }
+} 
